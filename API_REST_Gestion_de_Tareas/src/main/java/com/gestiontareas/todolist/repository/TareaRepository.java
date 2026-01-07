3b381974f45a06efd366dc992f5a721c7dfa5733
Alejandro Collado Severiano <colladoacs@gmail.com>
2026-01-07 23:40:59 +0100

    Add files via upload

--- API_REST_Gestion_de_Tareas/src/main/java/com/gestiontareas/todolist/repository/TareaRepository.java
+++ API_REST_Gestion_de_Tareas/src/main/java/com/gestiontareas/todolist/repository/TareaRepository.java
@@ -2,6 +2,8 @@ package com.gestiontareas.todolist.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.gestiontareas.todolist.model.Tarea;
+
 public interface TareaRepository extends JpaRepository<Tarea, Long> {
 
 }
