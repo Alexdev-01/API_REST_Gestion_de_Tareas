8fb682832c5feb55e6aba95223e157c4b65ccae0
Alejandro Collado Severiano <colladoacs@gmail.com>
2026-01-09 14:56:34 +0100

    Add files via upload

--- API_REST_Gestion_de_Tareas/src/main/java/com/gestiontareas/todolist/service/TareaServiceImpl.java
+++ API_REST_Gestion_de_Tareas/src/main/java/com/gestiontareas/todolist/service/TareaServiceImpl.java
@@ -2,10 +2,16 @@ package com.gestiontareas.todolist.service;
 
 import org.springframework.stereotype.Service;
 
+import com.gestiontareas.todolist.repository.TareaRepository;
+
 import lombok.RequiredArgsConstructor;
 
 @Service // Indica que esta clase es un servicio de Spring
 @RequiredArgsConstructor // Genera un constructor con los campos finales
-public class TareaServiceImpl {
+public class TareaServiceImpl implements TareaService {
+
+	private final TareaRepository tareaRepository;
+
+	
 
 }
