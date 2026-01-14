6dc608e031376031cc590f323d5ff583ff9cd9d2
Alejandro Collado Severiano <colladoacs@gmail.com>
2026-01-14 15:01:53 +0100

    Add files via upload

--- API_REST_Gestion_de_Tareas/src/main/java/com/gestiontareas/todolist/controller/TareaController.java
+++ API_REST_Gestion_de_Tareas/src/main/java/com/gestiontareas/todolist/controller/TareaController.java
@@ -1,12 +1,19 @@
 package com.gestiontareas.todolist.controller;
 
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.gestiontareas.todolist.model.Tarea;
 import com.gestiontareas.todolist.service.TareaService;
+
+import lombok.RequiredArgsConstructor;
 
 @RestController	// Indica que esta clase es un controlador REST de Spring
 @RequestMapping("/api/tareas")	// Define la ruta base para las solicitudes de este controlador
@@ -21,7 +28,17 @@ public class TareaController {
 		return tareaService.crearTarea(tarea);
 	}
 	
+	// Método para listar todas las tareas de un usuario específico
+	@GetMapping("/usuario/usuarioID")
+	public List<Tarea> listaPorUsuario(@PathVariable Long usuarioID) {	// @PathVariable indica que el valor de la variable de ruta se mapea al parámetro usuarioID
+		return tareaService.listarTareasPorUsuario(usuarioID);
+	}
 	
+	// Método para actualizar una tarea existente
+	@PutMapping("/{id}/estado")
+	public Tarea actualizar(@PathVariable  Long id, @RequestBody Tarea tarea) {	 // @PathVariable indica que el valor de la variable de ruta se mapea al parámetro id; @RequestBody indica que el cuerpo de la solicitud se mapea al objeto Tarea
+		return tareaService.actualizarTarea(id, tarea);
+	}
 	
 
 }
