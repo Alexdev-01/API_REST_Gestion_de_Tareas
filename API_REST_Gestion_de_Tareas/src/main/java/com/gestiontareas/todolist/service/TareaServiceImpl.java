0011422eed2050780ffce266bfe99051740c24f4
Alejandro Collado Severiano <colladoacs@gmail.com>
2026-01-15 15:17:03 +0100

    Add files via upload

--- API_REST_Gestion_de_Tareas/src/main/java/com/gestiontareas/todolist/service/TareaServiceImpl.java
+++ API_REST_Gestion_de_Tareas/src/main/java/com/gestiontareas/todolist/service/TareaServiceImpl.java
@@ -1,7 +1,12 @@
 package com.gestiontareas.todolist.service;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
+import com.gestiontareas.todolist.model.EstadoTarea;
+import com.gestiontareas.todolist.model.Tarea;
 import com.gestiontareas.todolist.repository.TareaRepository;
 
 import lombok.RequiredArgsConstructor;
@@ -12,6 +17,53 @@ public class TareaServiceImpl implements TareaService {
 
 	private final TareaRepository tareaRepository;
 
+	// Método para crear una nueva tarea
+	@Override
+	public Tarea crearTarea(Tarea tarea) {
+		tarea.setEstado(EstadoTarea.PENDIENTE); // Establece el estado inicial de la tarea como PENDIENTE
+		tarea.setFechaCreacion(LocalDateTime.now()); // Establece la fecha de creación actual
+		
+		return tareaRepository.save(tarea);
+	}
 	
+	// Método para listar todas las tareas de un usuario específico
+	@Override
+	public List<Tarea> listarTareasPorUsuario (Long usuarioId) {
+		return tareaRepository.findByUsuarioId(usuarioId);
+	}
+	
+	
+	// Método para actualizar una tarea existente
+	@Override
+	public Tarea actualizarTarea(Long id, Tarea tareaActualizada) {
+		Tarea tarea = tareaRepository.findById(id)
+				.orElseThrow(() -> new RuntimeException("Tarea no encontrada con id: " + id));
+		tarea.setTitulo(tareaActualizada.getTitulo());
+		tarea.setDescripcion(tareaActualizada.getDescripcion());
+		tarea.setEstado(tareaActualizada.getEstado());
+		
+		return tareaRepository.save(tarea);
+	}
+	
+	// Método para eliminar una tarea por su ID
+	@Override
+	public void elimarTarea(Long id) {
+		Tarea tarea = tareaRepository.findById(id)
+				.orElseThrow(() -> new RuntimeException("Tarea no encontrada con id: " + id));
+		tareaRepository.delete(tarea);		
+	}
+	
+	
+	// Método para cambiar el estado de una tarea
+	@Override
+	public Tarea cambiarEstado(Long id, EstadoTarea nuevoEstado) {
+		Tarea tarea = tareaRepository.findById(id)
+				.orElseThrow(() -> new RuntimeException("Tarea no encontrada con id: " + id));
+		tarea.setEstado(nuevoEstado);
+		
+		return tareaRepository.save(tarea);
+	}
 
+
+	
 }
